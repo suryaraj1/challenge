5e00696c4df6a75fd82684f8712802b371f03e8f
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-02-24 20:01:05 +0530

    completed the app

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ const crore = document.querySelector("input.crore");
 const million = document.querySelector("input.million");
 const rate = document.querySelector(".rate");
 
-const exchangeRate = () => {
+const exchangeRateINR_USD = () => {
   fetch(`https://api.exchangeratesapi.io/latest?base=USD&symbols=INR`)
     .then(response => response.json())
     .then(json => {
+      console.log("hello world");
       rate.innerHTML = json.rates.INR.toFixed(2);
-      million.value = crore.value * json.rates.INR;
+      million.value = ((crore.value / json.rates.INR) * 10).toFixed(2);
     });
 }
 
-exchangeRate();
+const exchangeRateUSD_INR = () => {
+  fetch(`https://api.exchangeratesapi.io/latest?base=USD&symbols=INR`)
+    .then(response => response.json())
+    .then(json => {
+      console.log("hello world");
+      rate.innerHTML = json.rates.INR.toFixed(2);
+      crore.value = ((million.value * json.rates.INR) / 10).toFixed(2);
+    });
+}
+
+
+crore.addEventListener('input', exchangeRateINR_USD);
+million.addEventListener('input', exchangeRateUSD_INR)
