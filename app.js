15530cca20a89f8e8eeadd807319b4932c4ce9cf
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-02-24 19:49:49 +0530

    updated js and added api url

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const crore = document.querySelector("input.crore");
 const million = document.querySelector("input.million");
+const rate = document.querySelector(".rate");
 
-console.log(crore, million);
+const exchangeRate = () => {
+  fetch(`https://api.exchangeratesapi.io/latest?base=USD&symbols=INR`)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      rate.innerHTML = json.rates.INR.toFixed(2);
+    });
+}
+
+exchangeRate();
